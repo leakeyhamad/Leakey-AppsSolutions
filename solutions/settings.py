c5b39ae5fbb6c2fdee171bb34eaeed0d94bc8fd6
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-22 11:06:42 +0300

    update settings file configurations

--- solutions/settings.py
+++ solutions/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
